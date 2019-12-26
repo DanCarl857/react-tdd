db64d31dde3c5053ec6a069dc00142f23c23700d
Daniel Carlson <dancarl758@gmail.com>
2019-12-26 07:32:09 +0100

    Added some BDD

--- gift-giver/src/components/App.test.js
+++ gift-giver/src/components/App.test.js
@@ -2,8 +2,35 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
 
-const app = shallow(<App />)
+describe('App', () => {
+    const app = shallow(<App />)
 
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot()
+    describe('Initialization', () => {
+        it('renders correctly', () => {
+            expect(app).toMatchSnapshot()
+        })
+    
+        it('initializes the `state` with an empty list of gifts', () => {
+            expect(app.state().gifts).toEqual([])
+        })
+    })
+
+    describe('When clicking the `add-gift` button', () => {
+
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click')
+        })
+
+        afterEach(() => {
+            app.setState({ gifts: [] })
+        })
+
+        it('adds a new gift to `state` when clicking the `add gift` button', () => {
+            expect(app.state().gifts).toEqual([{ id: 1}])
+        })
+    
+        it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        })
+    })
 })
